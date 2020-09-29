74f598c6b140d34b9cd3db493ce8b68d7709c58e
adeepa-hettiwattage <54160394+adeepa-hettiwattage@users.noreply.github.com>
2020-09-29 16:32:13 +1000

    Add files via upload

---
+++ Record part3/SwinAdventure/IdentifiableObject.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using SwinAdventure;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SwinAdventure
+{
+    public class IdentifiableObject
+    {
+        List<string> _identifier = new List<string>();
+        public IdentifiableObject(String[] Idents)
+        {
+            _identifier.AddRange(Idents);
+        }
+
+        public bool AreYou(string id)
+        {
+            foreach (string Check in _identifier)
+            {
+                if (id == Check)
+                { return true; }
+            }
+            return false;
+        }
+        public string FirstId
+        {
+            get
+            {
+                if (_identifier.Count == 0)
+                { return " "; }
+                else
+                { return _identifier.First(); }
+            }
+        }
+
+        public void AddIdentifier(string id)
+        {
+            _identifier.Add(id.ToLower());
+        }
+    }
+}
---
+++ Record part3/SwinAdventure/ItemTests.cs
@@ -0,0 +1,67 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SwinAdventure;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SwinAdventure.Tests
+{
+    [TestClass()]
+    public class ItemTests
+    {
+        /*
+         
+           [TestMethod()]
+        public void LongDescriptionTest()
+        {
+            Item id = new Item(new string[] { "potato", "baked" }, "baked potato", "a baked potato");
+            Assert.IsTrue(id.ShortDesc == ("A baked potato "));
+        }    
+             
+             
+         */
+        [TestMethod]
+        public void ItemIsIdentifiableTest()
+        {
+            Item id = new Item(new string[] { "potato", "baked" }, "baked patato", "a baked potato");
+
+            Assert.IsTrue(id.AreYou("potato"));
+            //potato?
+
+            Assert.IsTrue(id.AreYou("baked"));
+                //baked?
+        }
+
+
+
+        [TestMethod()]
+        public void LongDescriptionTest()
+        {
+            Item id = new Item(new string[] { "potato", "baked" }, "baked potato", "a baked potato");
+            // { "potato", "baked" }, "baked potato", "a baked potato");
+
+
+            Assert.IsTrue(id.ShortDesc == ("A baked potato "));
+        }
+
+
+        [TestMethod()]
+
+
+        public void ShortDescriptionTest()
+        {
+
+            /*
+              Item id = new Item(new string[] { "potato", "baked" }, "baked potato", "a baked potato");
+            Assert.IsTrue(id.ShortDesc == ("A baked potato patato"));*/
+
+            Item id = new Item(new string[] { "potato", "baked" }, "baked potato", "a baked potato");
+            Assert.IsTrue(id.ShortDesc == ("A baked potato patato"));//baked?
+        }
+
+      
+
+    }
+}
